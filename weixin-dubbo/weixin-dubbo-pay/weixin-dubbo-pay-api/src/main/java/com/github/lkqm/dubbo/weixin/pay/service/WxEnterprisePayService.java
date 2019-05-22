495b3f849e41a0e749b545363f9f2a3a4e9de832
Mario Luo <luokaiqiongmou@foxmail.com>
2019-05-23 00:37:47 +0800

    Complete enterprise pay implements

--- weixin-dubbo/weixin-dubbo-pay/weixin-dubbo-pay-api/src/main/java/com/github/lkqm/dubbo/weixin/pay/service/WxEnterprisePayService.java
+++ weixin-dubbo/weixin-dubbo-pay/weixin-dubbo-pay-api/src/main/java/com/github/lkqm/dubbo/weixin/pay/service/WxEnterprisePayService.java
@@ -1,7 +1,30 @@
 package com.github.lkqm.dubbo.weixin.pay.service;
 
+import com.github.binarywang.wxpay.bean.entpay.*;
+
 /**
  * 微信企业支付服务
  */
 public interface WxEnterprisePayService {
+
+    /**
+     * 企业付款
+     */
+    EntPayResult entPay(EntPayRequest request);
+
+    /**
+     * 查询企业付款
+     */
+    EntPayQueryResult queryEntPay(String partnerTradeNo);
+
+    /**
+     * 企业付款到银行卡
+     */
+    EntPayBankResult payBank(EntPayBankRequest request);
+
+    /**
+     * 企业付款到银行卡查询
+     */
+    EntPayBankQueryResult queryPayBank(String partnerTradeNo);
+
 }
--- weixin-dubbo/weixin-dubbo-pay/weixin-dubbo-pay-provider/src/main/java/com/github/lkqm/dubbo/weixin/pay/service/WxEnterpriseServiceImpl.java
+++ weixin-dubbo/weixin-dubbo-pay/weixin-dubbo-pay-provider/src/main/java/com/github/lkqm/dubbo/weixin/pay/service/WxEnterpriseServiceImpl.java
@@ -1,8 +1,11 @@
 package com.github.lkqm.dubbo.weixin.pay.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.github.binarywang.wxpay.bean.entpay.*;
 import com.github.binarywang.wxpay.service.EntPayService;
 import lombok.AllArgsConstructor;
+
+import static com.github.lkqm.dubbo.weixin.pay.util.WxHandlerUtils.handle;
 
 @Service
 @AllArgsConstructor
@@ -10,4 +13,23 @@ public class WxEnterpriseServiceImpl implements WxEnterprisePayService {
 
     private EntPayService entPayService;
 
+    @Override
+    public EntPayResult entPay(EntPayRequest request) {
+        return handle(() -> entPayService.entPay(request));
+    }
+
+    @Override
+    public EntPayQueryResult queryEntPay(String partnerTradeNo) {
+        return handle(() -> entPayService.queryEntPay(partnerTradeNo));
+    }
+
+    @Override
+    public EntPayBankResult payBank(EntPayBankRequest request) {
+        return handle(() -> entPayService.payBank(request));
+    }
+
+    @Override
+    public EntPayBankQueryResult queryPayBank(String partnerTradeNo) {
+        return handle(() -> entPayService.queryPayBank(partnerTradeNo));
+    }
 }
